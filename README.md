6e834d0bb39b171b8a98bf379f006c66e34069c8
しまぶー <0120koto@gmail.com>
2022-04-04 16:50:09 +0900

    🎉 Begin a project.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Next.js + Tailwind CSS Example
+
+- Next.js
+- Tailwind CSS
+- ESLint
+- Prettier
---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return <div className="text-blue-500">Hello!</div>;
+};
+
+export default Home;
